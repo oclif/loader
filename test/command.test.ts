bf6107b7d9e462d1cdabd40ad25343957975f5d8
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-28 15:23:04 -0800

    test: added extra test fixture

--- test/command.test.ts
+++ test/command.test.ts
@@ -1,25 +1,40 @@
-import {read} from '@dxcli/config'
+import {IPlugin, read} from '@dxcli/config'
 import {expect, fancy} from 'fancy-test'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
 import {load} from '../src'
 
-const root = path.join(__dirname, 'fixtures/typescript')
+const loadPlugin = (root: string) => async (ctx: {plugin: IPlugin}) => {
+  await reset(root)
+  ctx.plugin = await load({root})
+}
+
+const reset = async (root: string) => {
+  await fs.outputFile(path.join(root, '.git'), '')
+  const config = await read({root})
+  await fs.remove(config.cacheDir)
+  await fs.remove(config.dataDir)
+}
 
 describe('hooks', () => {
   fancy
-  .do(async () => {
-    await fs.outputFile(path.join(root, '.git'), '')
-    const config = await read({root})
-    await fs.remove(config.cacheDir)
-    await fs.remove(config.dataDir)
-  })
+  .do(loadPlugin(path.join(__dirname, 'fixtures/typescript')))
   .stdout()
-  .it('loads a TS command', async ctx => {
-    const plugin = await load({root})
-    const cmd = await plugin.commands[0].load()
+  .do(async ctx => {
+    const cmd = await ctx.plugin.commands[0].load()
     await cmd.run([])
-    expect(ctx.stdout).to.equal('loading plugins\nit works!\n')
+    expect(ctx.stdout).to.equal('it works!\n')
   })
+  .it('loads a TS plugin')
+
+  fancy
+  .do(loadPlugin(path.join(__dirname, 'fixtures/typescript2')))
+  .stdout()
+  .do(async ctx => {
+    const cmd = await ctx.plugin.commands[0].load()
+    await cmd.run([])
+    expect(ctx.stdout).to.equal('it works 2!\n')
+  })
+  .it('loads 2 TS plugins')
 })
