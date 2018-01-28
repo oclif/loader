ef299df282c1149241cf73b87e1cd59f75d56b2e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-28 15:38:01 -0800

    fix: make ts loader pull in extra roots

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,17 +1,27 @@
+import cli from 'cli-ux'
 import * as TSNode from 'ts-node'
 
+const rootDirs: string[] = []
+const typeRoots = [`${__dirname}/../node_modules/@types`]
+
 export function registerTSNode(debug: any, root: string) {
-  debug('registering ts-node at', root)
-  const tsNode: typeof TSNode = require('ts-node')
-  tsNode.register({
-    project: false,
-    // cache: false,
-    // typeCheck: true,
-    compilerOptions: {
-      target: 'esnext',
-      module: 'commonjs',
-      rootDirs: [`${root}/src`],
-      typeRoots: [`${__dirname}/../node_modules/@types`],
-    }
-  })
+  try {
+    debug('registering ts-node at', root)
+    const tsNode: typeof TSNode = require('ts-node')
+    typeRoots.push(`${root}/../node_modules/@types`)
+    rootDirs.push(`${root}/src`)
+    tsNode.register({
+      project: false,
+      // cache: false,
+      // typeCheck: true,
+      compilerOptions: {
+        target: 'esnext',
+        module: 'commonjs',
+        rootDirs,
+        typeRoots,
+      }
+    })
+  } catch (err) {
+    cli.warn(err)
+  }
 }
